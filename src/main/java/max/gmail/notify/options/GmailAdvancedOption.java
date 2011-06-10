cb079420713cb3dbf90482616aa67b32f402c9da
maxim.dybarskyy@gmail.com <maxim.dybarskyy@gmail.com@79645c28-23b4-378c-944c-5ee8ee85c7db>
2011-06-10 11:11:33 +0000

    7 platform version
    git-svn-id: http://netbeansgmailnotify.googlecode.com/svn/trunk@46 79645c28-23b4-378c-944c-5ee8ee85c7db

--- src/main/java/max/gmail/notify/options/GmailAdvancedOption.java
+++ src/main/java/max/gmail/notify/options/GmailAdvancedOption.java
@@ -6,14 +6,17 @@ import org.openide.util.NbBundle;
 
 public final class GmailAdvancedOption extends AdvancedOption {
 
+    @Override
     public String getDisplayName() {
         return NbBundle.getMessage(GmailAdvancedOption.class, "AdvancedOption_DisplayName_Plugin");
     }
 
+    @Override
     public String getTooltip() {
         return NbBundle.getMessage(GmailAdvancedOption.class, "AdvancedOption_Tooltip_Plugin");
     }
 
+    @Override
     public OptionsPanelController create() {
         return new GmailOptionsPanelController();
     }
