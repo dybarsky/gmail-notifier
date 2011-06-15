f127819a5725cc6d9888814ec710adbd4778984f
maxim.dybarskyy@gmail.com <maxim.dybarskyy@gmail.com@79645c28-23b4-378c-944c-5ee8ee85c7db>
2011-06-15 10:24:40 +0000

    cleanup
    git-svn-id: http://netbeansgmailnotify.googlecode.com/svn/trunk@58 79645c28-23b4-378c-944c-5ee8ee85c7db

--- src/main/java/max/gmail/notify/options/GmailOptionsPanel.java
+++ src/main/java/max/gmail/notify/options/GmailOptionsPanel.java
@@ -14,45 +14,43 @@ import max.gmail.notify.Notifier;
 import max.gmail.notify.settings.Settings;
 
 final class GmailOptionsPanel extends javax.swing.JPanel {
-
-    private final GmailOptionsPanelController controller;
-
+    
     private NumberFormatter format;
-
-    GmailOptionsPanel(GmailOptionsPanelController controller) {
-        this.controller = controller;
+    
+    GmailOptionsPanel() {
         initComponents();
-        format = new NumberFormatter(jTextField2, NumberFormat.getInstance(Locale.US));
+        format = new NumberFormatter(timeField, NumberFormat.getInstance(Locale.US));
         initGui();
     }
-
+    
     private void initGui() {
-        jTextField2.setEditable(false);
-        jTextField2.getCaret().setVisible(false);
-        jTextField2.setBackground(jTextField1.getBackground());
-
-        jTextField2.addKeyListener(format);
-        jTextField2.addFocusListener(new FocusAdapter() {
-
+        timeField.setEditable(false);
+        timeField.setBorder(userNameField.getBorder());
+        timeField.getCaret().setVisible(false);
+        timeField.setBackground(userNameField.getBackground());
+        
+        timeField.addKeyListener(format);
+        timeField.addFocusListener(new FocusAdapter() {
+            
             @Override
             public void focusGained(FocusEvent e) {
-                jTextField2.getCaret().setVisible(true);
+                timeField.getCaret().setVisible(true);
             }
-
+            
             @Override
             public void focusLost(FocusEvent e) {
-                jTextField2.getCaret().setVisible(false);
+                timeField.getCaret().setVisible(false);
             }
         });
         FocusListener focus = new FocusAdapter() {
-
+            
             @Override
             public void focusGained(FocusEvent e) {
                 JTextField f = (JTextField) e.getSource();
                 f.setSelectionStart(0);
                 f.setSelectionEnd(f.getText().length());
             }
-
+            
             @Override
             public void focusLost(FocusEvent e) {
                 JTextField f = (JTextField) e.getSource();
@@ -60,149 +58,151 @@ final class GmailOptionsPanel extends javax.swing.JPanel {
                 f.setSelectionEnd(0);
             }
         };
-        jTextField1.addFocusListener(focus);
-        jTextField2.addFocusListener(focus);
-        jTextField3.addFocusListener(focus);
-        jPasswordField1.addFocusListener(focus);
+        userNameField.addFocusListener(focus);
+        timeField.addFocusListener(focus);
+        folderField.addFocusListener(focus);
+        passwordField.addFocusListener(focus);
+        
+        
     }
-
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
-        jPasswordField1 = new javax.swing.JPasswordField();
-        jLabel4 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
-        jSeparator1 = new javax.swing.JSeparator();
+        userNameLabel = new javax.swing.JLabel();
+        exampleLabel = new javax.swing.JLabel();
+        userNameField = new javax.swing.JTextField();
+        passwordLabel = new javax.swing.JLabel();
+        passwordField = new javax.swing.JPasswordField();
+        timeLabel = new javax.swing.JLabel();
+        timeField = new javax.swing.JTextField();
+        minutes = new javax.swing.JLabel();
+        folderLabel = new javax.swing.JLabel();
+        folderField = new javax.swing.JTextField();
+        separator = new javax.swing.JSeparator();
 
-        org.openide.awt.Mnemonics.setLocalizedText(jLabel1, "Full user name");
+        org.openide.awt.Mnemonics.setLocalizedText(userNameLabel, "Full user name");
 
-        jLabel2.setFont(new java.awt.Font("Dialog", 0, 10)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(153, 153, 153));
-        org.openide.awt.Mnemonics.setLocalizedText(jLabel2, "(user@gmail.com)");
+        exampleLabel.setFont(new java.awt.Font("Dialog", 0, 10)); // NOI18N
+        exampleLabel.setForeground(new java.awt.Color(153, 153, 153));
+        org.openide.awt.Mnemonics.setLocalizedText(exampleLabel, "(user@gmail.com)");
 
-        org.openide.awt.Mnemonics.setLocalizedText(jLabel3, "Password");
+        org.openide.awt.Mnemonics.setLocalizedText(passwordLabel, "Password");
 
-        org.openide.awt.Mnemonics.setLocalizedText(jLabel4, "Update time");
+        org.openide.awt.Mnemonics.setLocalizedText(timeLabel, "Update time");
 
-        jLabel5.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
-        org.openide.awt.Mnemonics.setLocalizedText(jLabel5, "(min)");
+        minutes.setFont(new java.awt.Font("Dialog", 0, 12));
+        org.openide.awt.Mnemonics.setLocalizedText(minutes, "(min)");
 
-        org.openide.awt.Mnemonics.setLocalizedText(jLabel6, "Check folder");
+        org.openide.awt.Mnemonics.setLocalizedText(folderLabel, "Check folder");
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(layout.createSequentialGroup()
+                .add(12, 12, 12)
+                .add(separator, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 517, Short.MAX_VALUE))
+                .add(12, 12, 12)
+            .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(jSeparator1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 484, Short.MAX_VALUE)
+                    .add(userNameLabel)
                     .add(layout.createSequentialGroup()
-                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                            .add(jLabel1)
-                            .add(layout.createSequentialGroup()
-                                .add(jLabel4)
-                                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                                .add(jLabel5))
-                            .add(jLabel3)
-                            .add(jLabel6))
+                        .add(timeLabel)
                         .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                            .add(layout.createSequentialGroup()
-                                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
-                                    .add(org.jdesktop.layout.GroupLayout.LEADING, jTextField3, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 156, Short.MAX_VALUE)
-                                    .add(org.jdesktop.layout.GroupLayout.LEADING, jTextField1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 156, Short.MAX_VALUE)
-                                    .add(jPasswordField1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 156, Short.MAX_VALUE))
-                                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                                .add(jLabel2)
-                                .addContainerGap(89, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                            .add(layout.createSequentialGroup()
-                                .add(jTextField2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 65, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                                .addContainerGap())))))
+                        .add(minutes))
+                    .add(passwordLabel)
+                    .add(folderLabel))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 17, Short.MAX_VALUE)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(folderField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 236, Short.MAX_VALUE)
+                    .add(timeField, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 65, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                    .add(passwordField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 236, Short.MAX_VALUE)
+                    .add(userNameField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 236, Short.MAX_VALUE))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(exampleLabel)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(layout.createSequentialGroup()
                 .addContainerGap()
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(jLabel1)
-                    .add(jTextField1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(jLabel2))
+                    .add(userNameLabel)
+                    .add(userNameField, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                    .add(exampleLabel))
                 .add(18, 18, 18)
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(jPasswordField1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(jLabel3))
-                .add(18, 18, 18)
-                .add(jSeparator1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                    .add(passwordLabel)
+                    .add(passwordField, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                .add(20, 20, 20)
+                .add(separator, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                 .add(14, 14, 14)
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(jLabel4)
-                    .add(jTextField2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(jLabel5))
+                    .add(timeLabel)
+                    .add(minutes)
+                    .add(timeField, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                 .add(18, 18, 18)
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(jLabel6)
-                    .add(jTextField3, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(88, Short.MAX_VALUE))
+                    .add(folderField)
+                    .add(folderLabel))
+                .add(18, 18, 18)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING, false))
+                .add(34, 34, 34))
         );
-    }// </editor-fold>//GEN-END:initComponents
-
+    }// </editor-fold>                        
+    
     void load() {
-
         Settings set = Settings.load();
-
+        
         if (set != null && set.getUser() != null) {
-            jTextField1.setText(set.getUser());
+            userNameField.setText(set.getUser());
         }
         if (set != null && set.getPass() != null) {
-            jPasswordField1.setText(set.getPass());
+            passwordField.setText(set.getPass());
         }
         if (set != null && set.getFolderName() != null) {
-            jTextField3.setText(set.getFolderName());
+            folderField.setText(set.getFolderName());
         } else {
-            jTextField3.setText("inbox");
+            folderField.setText("inbox");
         }
         if (set != null) {
-            jTextField2.setText(String.valueOf((double) set.getDelay() / 1000 / 60));
+            timeField.setText(String.valueOf((double) set.getDelay() / 1000 / 60));
         } else {
-            jTextField2.setText("0");
+            timeField.setText("0");
         }
     }
-
+    
     void store() {
         Settings set = new Settings();
-        set.setUser(jTextField1.getText());
-        set.setPass(new String(jPasswordField1.getPassword()));
-        set.setFolderName(jTextField3.getText());
+        set.setUser(userNameField.getText());
+        set.setPass(new String(passwordField.getPassword()));
+        set.setFolderName(folderField.getText());
         set.setDelay((int) (format.getValue().doubleValue() * 60 * 1000));
-
+        
         Settings.save(set);
-
+        
         Notifier.stop();
         Notifier.start();
     }
-
+    
     boolean valid() {
         return true;
     }
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    // End of variables declaration//GEN-END:variables
+    
+    // Variables declaration - do not modify                     
+    private javax.swing.JLabel exampleLabel;
+    private javax.swing.JTextField folderField;
+    private javax.swing.JLabel folderLabel;
+    private javax.swing.JLabel minutes;
+    private javax.swing.JPasswordField passwordField;
+    private javax.swing.JLabel passwordLabel;
+    private javax.swing.JSeparator separator;
+    private javax.swing.JTextField timeField;
+    private javax.swing.JLabel timeLabel;
+    private javax.swing.JTextField userNameField;
+    private javax.swing.JLabel userNameLabel;
+    // End of variables declaration                   
 }
